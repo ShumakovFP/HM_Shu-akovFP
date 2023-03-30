cb2add456dfa8969c3667cb1dee04acf8e03299a
Shumakov F.P <Grodogad@gmail.com>
2023-03-30 22:32:02 +0700

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int N2 = int.Parse(Console.ReadLine());
+if (N1 > N2)
+{
+    Console.WriteLine("Большее число");
+    Console.WriteLine(N1);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(N2);
+}
+else
+{
+    Console.WriteLine("Большее число");
+    Console.WriteLine(N2);
+    Console.WriteLine("Меньшее число");
+    Console.WriteLine(N1);
+}
