f5f095a681d9446483788a7a9c424cef476f349a
Shumakov F.P <Grodogad@gmail.com>
2023-03-30 22:49:11 +0700

    add Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+Console.WriteLine("Введите 1 число: ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int N2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3 число: ");
+int N3 = int.Parse(Console.ReadLine());
+
+if (N1 > max)
+{
+    max = N1;
+}
+if (N2 > max)
+{
+    max = N2;
+}
+if (N3 > max)
+{
+   max = N3;
+}
+Console.WriteLine(max);
